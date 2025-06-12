f94be0477058aeed434ebc290539d7b04ea650c7
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-06-12 14:19:44 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "GOTAR~XMD~bpBmzYib#1VRya7A4pPNYb58-QLzVF2S_qzkz2YtAkXPbCCd9s1M",
+SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "iyoo whats up 💫",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
